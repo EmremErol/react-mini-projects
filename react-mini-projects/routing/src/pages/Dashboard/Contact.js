a18944fb2d7695fc1a417167a0ad5e2685eb02bb
Emre Erol <emreerol630@gmail.com>
2026-01-19 02:00:04 +0300

    Improve contact form with submit state and reset

--- react-mini-projects/routing/src/pages/Dashboard/Contact.js
+++ react-mini-projects/routing/src/pages/Dashboard/Contact.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { useFormik } from 'formik';
 
 function Contact() {
-  const {handleSubmit, handleChange} = useFormik({
+  const {handleSubmit, handleChange, values, isSubmitting} = useFormik({
     initialValues:{
         firstName: '',
         lastName: '',
         email: '',
         message:'',
       },
-      onSubmit:(values) => {
+      onSubmit: async(values, bag) => {
+        await new Promise((r) => setTimeout(r, 1000));
         console.log(values);
+
+        bag.resetForm();
       }
   })
   return (
@@ -21,11 +24,25 @@ function Contact() {
         <form onSubmit={handleSubmit} className="form">
           <div>
         <label htmlFor="firstName">First Name</label>
-        <input id="firstName" name="firstName" placeholder="Jane" onChange={handleChange('firstName')} />
+        <input 
+            id="firstName" 
+            name="firstName"
+            placeholder="Jane" 
+            disabled={isSubmitting}
+            onChange={handleChange('firstName')
+
+        }  value={values.firstName}/>
           </div>
           <div>
         <label htmlFor="lastName">Last Name</label>
-        <input id="lastName" name="lastName" placeholder="Doe" onChange={handleChange('lastName')} />
+        <input 
+            id="lastName"
+            name="lastName"
+            placeholder="Doe"
+            disabled={isSubmitting}
+            onChange={handleChange('lastName')
+
+             } value={values.lastName} />
           </div>
           <div>
         
@@ -34,6 +51,8 @@ function Contact() {
           name="email"
           placeholder="jane@acme.com"
           type="email"
+          value={values.email}
+          disabled={isSubmitting}
           onChange={handleChange('email')}
         />
         </div>
@@ -43,11 +62,13 @@ function Contact() {
           id="message"
           name="message"
           placeholder="your Message ..."
-          type="email"
+          type="text"
+          value={values.message}
+          disabled={isSubmitting}
           onChange={handleChange('message')}
         />
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit" disabled={isSubmitting}>Submit</button>
       </form>
       
 
